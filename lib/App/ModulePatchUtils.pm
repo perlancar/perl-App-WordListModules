7883b90f241a7dbe60c2e0256730e812df591f50
perlancar (@pc-office) <perlancar@gmail.com>
2016-06-09 02:05:24 +0700

    Initial commit

---
+++ lib/App/ModulePatchUtils.pm
@@ -0,0 +1,23 @@
+package App::ModulePatchUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+# ABSTRACT: Collection of CLI utilities related to Module::Patch
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities related to
+L<Module::Patch>:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Module::Patch>
+
+=cut
